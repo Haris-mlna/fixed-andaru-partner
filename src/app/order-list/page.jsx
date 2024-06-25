f4465b57603d126b4d4e4b46ae22bf4773c209f6
Ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2024-06-25 10:40:14 +0700

    change language to indonesia in daftar pesanan & table daftar pesanan

--- src/app/order-list/page.jsx
+++ src/app/order-list/page.jsx
@@ -12,141 +12,143 @@ import { GoTrash } from "react-icons/go";
 import Swal from "sweetalert2";
 
 const OrderList = () => {
-	const { user } = useUser();
-	const { setDetail, setDetailList } = useOrderDetail();
-	const [loading, setLoading] = React.useState(false);
-	const [selected, setSelected] = React.useState([]);
-	const [list, setList] = React.useState([]);
-	const [cancelLoading, setCancelLoading] = React.useState(false);
-	const [update, setUpdate] = React.useState(false);
-	const router = useRouter();
+  const { user } = useUser();
+  const { setDetail, setDetailList } = useOrderDetail();
+  const [loading, setLoading] = React.useState(false);
+  const [selected, setSelected] = React.useState([]);
+  const [list, setList] = React.useState([]);
+  const [cancelLoading, setCancelLoading] = React.useState(false);
+  const [update, setUpdate] = React.useState(false);
+  const router = useRouter();
 
-	const fetchList = async id => {
-		setLoading(true);
-		try {
-			const res = await getListorder(id);
-			if (res) {
-				const dataWithId = res.data.map(item => ({
-					...item,
-					id: item.Id,
-				}));
-				setList(dataWithId);
-			}
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const fetchList = async (id) => {
+    setLoading(true);
+    try {
+      const res = await getListorder(id);
+      if (res) {
+        const dataWithId = res.data.map((item) => ({
+          ...item,
+          id: item.Id,
+        }));
+        setList(dataWithId);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	React.useEffect(() => {
-		if (user) {
-			fetchList(user.OrganizationId);
-		}
-	}, [user]);
+  React.useEffect(() => {
+    if (user) {
+      fetchList(user.OrganizationId);
+    }
+  }, [user]);
 
-	const handleDetail = async (item, event) => {
-		event.stopPropagation(); // Prevent event propagation
-		setDetail(item);
-		try {
-			const res = await orderListDetail(item.Id);
-			setLoading(true);
-			if (res) {
-				setDetailList(res.data);
-				setTimeout(() => {
-					router.push("/order-list/detail-order");
-				}, 1000);
-			}
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setLoading(false);
-		}
-	};
+  const handleDetail = async (item, event) => {
+    event.stopPropagation(); // Prevent event propagation
+    setDetail(item);
+    try {
+      const res = await orderListDetail(item.Id);
+      setLoading(true);
+      if (res) {
+        setDetailList(res.data);
+        setTimeout(() => {
+          router.push("/order-list/detail-order");
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	const handleCancelOrder = async () => {
-		const result = await Swal.fire({
-			title: "Warning!",
-			text: "Are you sure you want to cancel the selected orders?",
-			icon: "warning",
-			showCancelButton: true,
-			confirmButtonText: "Yes, cancel it!",
-			cancelButtonText: "No, keep it",
-		});
+  const handleCancelOrder = async () => {
+    const result = await Swal.fire({
+      title: "Warning!",
+      text: "Anda yakin ingin membatalkan pesanan ini?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Ya, batalkan!",
+      cancelButtonText: "Tidak",
+    });
 
-		if (result.isConfirmed) {
-			setCancelLoading(true);
-			try {
-				const res = await CancelOrder(selected);
-				if (res) {
-					Swal.fire(
-						"Cancelled!",
-						"Your orders have been cancelled.",
-						"success"
-					);
-					// Optionally, refresh the order list here
-					if (user) {
-						fetchList(user.OrganizationId);
-					}
-					setSelected([]); // Clear selected items
-				}
-			} catch (error) {
-				Swal.fire(
-					"Error!",
-					"Failed to cancel orders. Please try again.",
-					"error"
-				);
-			} finally {
-				setCancelLoading(false);
-			}
-		}
-	};
+    if (result.isConfirmed) {
+      setCancelLoading(true);
+      try {
+        const res = await CancelOrder(selected);
+        if (res) {
+          Swal.fire(
+            "Cancelled!",
+            "Your orders have been cancelled.",
+            "success"
+          );
+          // Optionally, refresh the order list here
+          if (user) {
+            fetchList(user.OrganizationId);
+          }
+          setSelected([]); // Clear selected items
+        }
+      } catch (error) {
+        Swal.fire(
+          "Error!",
+          "Failed to cancel orders. Please try again.",
+          "error"
+        );
+      } finally {
+        setCancelLoading(false);
+      }
+    }
+  };
 
-	return (
-		<div className='w-full h-screen flex'>
-			<Sidebar />
-			{cancelLoading && (
-				<div
-					style={{
-						zIndex: 1000,
-					}}
-					className='w-full h-screen absolute bg-black/40 flex justify-center items-center'>
-					<CircularProgress
-						size={20}
-						sx={{
-							color: "#FFF",
-						}}
-					/>
-				</div>
-			)}
-			<div className='flex relative flex-1 bg-white'>
-				<div className='w-full overflow-y-auto'>
-					<div className='w-full h-12 flex items-center px-4 bg-white shadow-sm'>
-						Order List
-					</div>
-					{selected && selected?.length > 0 && (
-						<div className='p-4 flex'>
-							<button
-								className='p-1 px-8 text-red-500 bg-red-100 border border-red-300 rounded flex gap-1 items-center'
-								onClick={handleCancelOrder}
-								disabled={cancelLoading}>
-								<GoTrash />
-								Cancel Orders
-							</button>
-						</div>
-					)}
-					<div className='w-full'>
-						<TableOrderList
-							rows={list}
-							handleDetail={handleDetail}
-							selected={selected}
-							setSelected={setSelected}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-screen flex">
+      <Sidebar />
+      {cancelLoading && (
+        <div
+          style={{
+            zIndex: 1000,
+          }}
+          className="w-full h-screen absolute bg-black/40 flex justify-center items-center"
+        >
+          <CircularProgress
+            size={20}
+            sx={{
+              color: "#FFF",
+            }}
+          />
+        </div>
+      )}
+      <div className="flex relative flex-1 bg-white">
+        <div className="w-full overflow-y-auto">
+          <div className="w-full h-12 flex items-center px-4 bg-white shadow-sm">
+            Daftar Pesanan
+          </div>
+          {selected && selected?.length > 0 && (
+            <div className="p-4 flex">
+              <button
+                className="p-1 px-8 text-red-500 bg-red-100 border border-red-300 rounded flex gap-1 items-center"
+                onClick={handleCancelOrder}
+                disabled={cancelLoading}
+              >
+                <GoTrash />
+                Batalkan Pesanan
+              </button>
+            </div>
+          )}
+          <div className="w-full">
+            <TableOrderList
+              rows={list}
+              handleDetail={handleDetail}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderList;
--- src/components/ui/table/table.order-list.jsx
+++ src/components/ui/table/table.order-list.jsx
@@ -6,75 +6,75 @@ import { DataGrid } from "@mui/x-data-grid";
 import moment from "moment";
 
 export default function TableOrderList(props) {
-	const { rows, handleDetail, setSelected } = props;
+  const { rows, handleDetail, setSelected } = props;
 
-	const [rowSelectionModel, setRowSelectionModel] = useState([]);
+  const [rowSelectionModel, setRowSelectionModel] = useState([]);
 
-	// Ensure rows array has at least 10 items
-	const displayRows =
-		rows.length < 10 ? [...rows] : rows;
+  // Ensure rows array has at least 10 items
+  const displayRows = rows.length < 10 ? [...rows] : rows;
 
-	const columns = [
-		{ field: "OrderNumber", headerName: "Order Number", flex: 1 },
-		{ field: "InsertedBy", headerName: "Inserted By", flex: 1 },
-		{ field: "SupplierName", headerName: "Supplier Name", flex: 1 },
-		{ field: "DeliveryAddress", headerName: "Delivery Address", flex: 2 },
-		{
-			field: "Date",
-			headerName: "Date",
-			flex: 1,
-			renderCell: params => (
-				<div>{params.value ? moment(params.value).format("ll") : ""}</div>
-			),
-		},
-		{
-			field: "ExpectedDeliveryDate",
-			headerName: "Expected Delivery Date",
-			flex: 1,
-			renderCell: params => (
-				<div>{params.value ? moment(params.value).format("ll") : ""}</div>
-			),
-		},
-		{ field: "Status", headerName: "Status", flex: 1 },
-		{
-			field: "actions",
-			headerName: "Actions",
-			flex: 1.5, // Increase the flex value to give more space for the button
-			renderCell: params => (
-				<div className='w-full h-full flex justify-center items-center'>
-					<button
-						onClick={event => handleDetail(params.row, event)} // Pass event parameter
-						className='border-blue-500 text-blue-500 bg-blue-50 rounded-full h-8 w-20 font-outfit flex justify-center items-center'>
-						Detail
-					</button>
-				</div>
-			),
-		},
-	];
+  const columns = [
+    { field: "OrderNumber", headerName: "Nomor Pesanan", flex: 1 },
+    { field: "InsertedBy", headerName: "PIC", flex: 1 },
+    { field: "SupplierName", headerName: "Nama Supplier", flex: 1 },
+    { field: "DeliveryAddress", headerName: "Alamat Pengiriman", flex: 2 },
+    {
+      field: "Date",
+      headerName: "Tanggal",
+      flex: 1,
+      renderCell: (params) => (
+        <div>{params.value ? moment(params.value).format("ll") : ""}</div>
+      ),
+    },
+    {
+      field: "ExpectedDeliveryDate",
+      headerName: "Perkiraan Sampai",
+      flex: 1,
+      renderCell: (params) => (
+        <div>{params.value ? moment(params.value).format("ll") : ""}</div>
+      ),
+    },
+    { field: "Status", headerName: "Status", flex: 1 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      flex: 1.5, // Increase the flex value to give more space for the button
+      renderCell: (params) => (
+        <div className="w-full h-full flex justify-center items-center">
+          <button
+            onClick={(event) => handleDetail(params.row, event)} // Pass event parameter
+            className="border-blue-500 text-blue-500 bg-blue-50 rounded-full h-8 w-20 font-outfit flex justify-center items-center"
+          >
+            Detail
+          </button>
+        </div>
+      ),
+    },
+  ];
 
-	const handleSelectionChange = newRowSelectionModel => {
-		setRowSelectionModel(newRowSelectionModel);
-		const selectedRows = newRowSelectionModel.map(id =>
-			displayRows.find(row => row.id === id)
-		);
-		setSelected(newRowSelectionModel);
-	};
+  const handleSelectionChange = (newRowSelectionModel) => {
+    setRowSelectionModel(newRowSelectionModel);
+    const selectedRows = newRowSelectionModel.map((id) =>
+      displayRows.find((row) => row.id === id)
+    );
+    setSelected(newRowSelectionModel);
+  };
 
-	return (
-		<div style={{ width: "100%", height: 640 }}>
-			<DataGrid
-				rows={displayRows}
-				columns={columns}
-				onRowSelectionModelChange={handleSelectionChange}
-				rowSelectionModel={rowSelectionModel}
-				initialState={{
-					pagination: {
-						paginationModel: { page: 0, pageSize: 10 },
-					},
-				}}
-				className='font-outfit'
-				checkboxSelection
-			/>
-		</div>
-	);
+  return (
+    <div style={{ width: "100%", height: 640 }}>
+      <DataGrid
+        rows={displayRows}
+        columns={columns}
+        onRowSelectionModelChange={handleSelectionChange}
+        rowSelectionModel={rowSelectionModel}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 10 },
+          },
+        }}
+        className="font-outfit"
+        checkboxSelection
+      />
+    </div>
+  );
 }
